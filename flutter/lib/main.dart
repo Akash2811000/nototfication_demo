1f8c4da005d79617a607f4168b5f5dbad83dfbdf
Akash2811000 <akashgupta7383@gmail.com>
2023-02-05 10:12:13 +0530

    get error from firebase

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -2,8 +2,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:notification_demo/features/login/presentation/pages/login.dart';
 import './features/login/login_injection.dart' as loginsl ;
-import 'firebase_options.dart'
+
 import './core/navigation/app_router.dart' as app_routes;
+import 'firebase_options.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await loginsl.init();
