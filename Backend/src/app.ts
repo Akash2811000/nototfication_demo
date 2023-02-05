7682faa5deaa9b0255f59525d1effe09e79c2a97
Akash2811000 <akashgupta7383@gmail.com>
2023-02-05 10:42:03 +0530

    admin

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -2,10 +2,18 @@ import express, { Express, Request, Response } from 'express';
 import * as dotenv from 'dotenv';
 dotenv.config();
 import { LoggerMiddleware } from './middleware/logger';
+import * as admin from 'firebase-admin';
+import credential from "../notofication-demo-ccf50-firebase-adminsdk-ujm95-c534dd31ec.json";
 const app: Express = express();
 app.use(express.json());
 
 import { router as userroute } from './controller/login_contoller';
+// FIREBASE INTITIALIZE
+admin.initializeApp(
+    {
+        credential: admin.credential.cert(JSON.parse(JSON.stringify(credential)))
+    }
+);
 
 app.use(LoggerMiddleware);
 // ROOT LEVEL
